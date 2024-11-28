5c63f05550dab701a904874dc8d7336f3f1b9f4e
John Ivan Respende <JIRespende@gmail.com>
2024-11-29 05:12:24 +0800

    Add files via upload

---
+++ visualizations/models.py
@@ -0,0 +1,210 @@
+from django.db import models
+
+class account1(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account1'
+
+class account2(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account2'
+
+class account3(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account3'
+
+class account6(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account6'
+
+class account7(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account7'
+
+class account8(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account8'
+
+class account9(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account9'
+
+class account10(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account10'
+
+class account13(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account13'
+
+class account12(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account12'
+
+class account11(models.Model):
+    symbol = models.CharField(max_length=50)
+    orderId = models.BigIntegerField()
+    side = models.CharField(max_length=10)
+    price = models.TextField()
+    qty = models.TextField()
+    realizedPnl = models.TextField()
+    marginAsset = models.TextField()
+    quoteQty = models.TextField()
+    commission = models.TextField()
+    commissionAsset = models.TextField()
+    time = models.DateTimeField()
+    positionSide = models.TextField()
+    buyer = models.BooleanField()
+    maker = models.BooleanField()
+    
+    class Meta:
+        db_table = 'account11'
